2c1e1e7470284fa95cc6f9eb6cdbce27fe842190
Amir Mohajerani <amohajerani6@gmail.com>
2023-01-09 00:03:08 -0500

    none

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ const providerConfig = {
   domain: config.domain,
   clientId: config.clientId,
   ...(config.audience ? { audience: config.audience } : null),
-  redirectUri: "/dashboard",
+  redirectUri: "https://thegagali.com/dashboard",
   onRedirectCallback,
 }
 
