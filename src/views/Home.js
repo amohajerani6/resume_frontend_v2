413bf468f51deef093bd00242fc98f8c8fc0f859
Amir Mohajerani <amohajerani6@gmail.com>
2023-01-09 00:19:27 -0500

    none

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,12 +1,20 @@
 import React, { Fragment } from "react"
+import { useAuth0 } from "@auth0/auth0-react"
 
 import Hero from "../components/Hero"
 
-const Home = () => (
-  <Fragment>
-    <hr />
-    <Hero />
-  </Fragment>
-)
+const Home = () => {
+  const { user, isAuthenticated, loginWithRedirect, logout } = useAuth0()
+
+  if (!isAuthenticated) {
+    return (
+      <Fragment>
+        <hr />
+        <Hero />
+      </Fragment>
+    )
+  }
+  return <h2>logged in</h2>
+}
 
 export default Home
