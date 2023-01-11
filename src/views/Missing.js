3745d641fee18568a9e717901cf63edac6a200b9
Amir Mohajerani <amohajerani6@gmail.com>
2023-01-10 21:41:20 -0500

    a

--- src/views/Missing.js
+++ src/views/Missing.js
@@ -2,10 +2,10 @@ import React from "react"
 
 const Missing = () => {
   return (
-    <article style={{ padding: "100px" }}>
+    <div style={{ padding: "100px" }}>
       <h1>Oops!</h1>
       <p>Page Not Found</p>
-    </article>
+    </div>
   )
 }
 
